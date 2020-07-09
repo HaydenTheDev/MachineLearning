e4dcc8c99e0da0547f2855da0f6e614678d32546
Hayden Bazor <hbazor@murraystate.edu>
2020-07-09 17:08:31 -0500

    starting with Simple linear regression, added dataset as well as Splitting the data

--- simpleregression.py
+++ simpleregression.py
@@ -2,15 +2,24 @@
 #  example                      salary = b0 + b1 * experience
 
 
+#  Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#  Importing the libraries
-
 #  Importing the dataset
 
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
 #  Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                    test_size=0.2,
+                                                    random_state=1)
 
 #  Training the Simple Linear Regression model on the Training set
 
