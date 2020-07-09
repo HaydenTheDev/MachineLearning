e3afde08416a4fe01afa607ff6d3d7b48fea46df
Hayden Bazor <hbazor@murraystate.edu>
2020-07-09 15:36:26 -0500

    Encoding the Independent Variable

--- data_preprocessing_tools.py
+++ data_preprocessing_tools.py
@@ -20,8 +20,17 @@ x[:, 1:3] = imputer.transform(x[:, 1:3])
 print(x)
 
 # Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])],
+                       remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+print(x)
 # Encoding the Independent Variable
-
+from sklearn.preprocessing import LabelEncoder
+lb = LabelEncoder()
+y = lb.fit_transform(y)
+print(y)
 # Encoding the Dependent Variable
 
 
