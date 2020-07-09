8802df3a02c160b61b6b2fda21621ff68453797b
Hayden Bazor <hbazor@murraystate.edu>
2020-07-09 17:03:50 -0500

    starting with Simple linear regression

---
+++ simpleregression.py
@@ -0,0 +1,21 @@
+#  simple regression fromula // y= b0 + b1 * x1
+#  example                      salary = b0 + b1 * experience
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#  Importing the libraries
+
+#  Importing the dataset
+
+#  Splitting the dataset into the Training set and Test set
+
+#  Training the Simple Linear Regression model on the Training set
+
+#  Predicting the Test set results
+
+#  Visualizing the Training set results
+
+#  Visualizing the Test set results
