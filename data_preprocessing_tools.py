1fa455321f36340e037a75f02f7e834b9760e88e
Hayden Bazor <hbazor@murraystate.edu>
2020-07-09 14:53:58 -0500

    Data preprocessing with Python ML

---
+++ data_preprocessing_tools.py
@@ -0,0 +1,23 @@
+# Data Preprocessing Tools
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+
+# Taking care of missing data
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+
+# Encoding the Dependent Variable
+
+
+# Splitting the dataset into the Training set and Test set
+
+# Feature Scaling
