1dad0e9b596e0c486f3998eed569b4e5b7b06fbe
Hayden Bazor <hbazor@murraystate.edu>
2020-07-09 18:14:21 -0500

    starting with Simple linear regression graph.

--- simpleregression.py
+++ simpleregression.py
@@ -26,11 +26,22 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+print("x train set: ")
 print(x_train)
+print("y train set: ")
 print(y_train)
 
 #  Predicting the Test set results
 
+y_pred = regressor.predict(x_test)
+
 #  Visualizing the Training set results
+
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title("Salary vs Experience (Training set)")
+plt.xlabel("Years of experience")
+plt.ylabel("Salary")
+plt.show()
 
 #  Visualizing the Test set results
